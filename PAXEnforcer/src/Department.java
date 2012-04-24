6e7fe00eca1f1c849146bf0e235581bed1b76fdb
Kevin Chang <Kevin.w.chang@gmail.com>
2012-04-24 00:32:48 -0400

    Added helper methods to Enforcer.java. Also added testController for future testing.

--- PAXEnforcer/src/Department.java
+++ PAXEnforcer/src/Department.java
@@ -1,3 +1,4 @@
+//Specific department at PAX (ex. Line Entertainment)
 
 public class Department {
 	public Department()
--- PAXEnforcer/src/Enforcer.java
+++ PAXEnforcer/src/Enforcer.java
@@ -1,31 +1,75 @@
+import java.util.StringTokenizer;
+
 
 public class Enforcer {
 
 	private String handle;
 	private String[] depPref;
 	private Department myDepartment;
+	private int depPrefNum = 5;
 	
 	public Enforcer()
 	{
-		setDepPref(new String[5]);
+		setDepPref(new String[depPrefNum]);
 		setHandle ("Enforcer");
 		myDepartment = null;
 	}
 	
+	//add a list of departments based upon a string
+	public void addDepPref(String depPrefString)
+	{
+		try {
+			this.depPref = depConvert(depPrefString);
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
 	public String toString()
 	{
+		//Output handle
 		String output = "Handle: " + getHandle() + " ";
 		
+		//Output Department Preferences
 		output += "Department Preferences: ";
-		for (int i=0;i<depPref.length;i++)
+		for (int i=0;i<depPref.length-1;i++)
 		{
-			output+= depPref[i] +",";
+			output+= depPref[i];
+			if (i+1<depPref.length-1)
+				{output+=",";}
 		}
+		output += " ";
 		
+		//Output 
 		return output;
 	}
 	
-
+	/*
+	 * Converts a string into the array of departments.
+	 * Currently mandates that the array has to match the preset depPrefNum
+	 * Consider changing so people can put in null preferences...?
+	 *  
+	*/
+	private String[] depConvert(String depString) throws Exception
+	{
+		String depConvert[] =new String[depPrefNum];
+		StringTokenizer myTokenizer = new StringTokenizer(depString);
+		if(myTokenizer.countTokens()==depPrefNum){
+			throw new Exception("Departments in the string does not match " +
+					"Department Prefr Number of " + depPrefNum);
+		}
+		else
+		{
+			for (int i= 0; i<depPrefNum-1; i++)
+			{
+				depConvert[i]=myTokenizer.nextToken();
+			}
+		}
+		
+		return depConvert;
+	}
+	
 	public String[] getDepPref() {
 		return depPref;
 	}
@@ -40,6 +84,16 @@ public class Enforcer {
 
 	public void setHandle(String handle) {
 		this.handle = handle;
+	}
+	
+	public void setDepartment (Department newDep)
+	{
+		this.myDepartment = newDep;
+	}
+	
+	public Department getDepartment()
+	{
+		return myDepartment;
 	}
 
 }
