130c966fcc35a990b2151a8fec4ab261983c7dec
Kevin Chang <Kevin.w.chang@gmail.com>
2012-04-26 00:26:47 -0400

    Add SchedulerController placeholder

---
+++ PAXEnforcer/src/schedulerController.java
@@ -0,0 +1,17 @@
+/*
+ * Overall controller to handle managing all departments (and subsequently all enforcers)
+ * 
+ * Will possibly eventually control gui but definitely needs to contorl File I/O
+ */
+
+public class schedulerController {
+
+	/**
+	 * @param args
+	 */
+	public schedulerController()
+	{
+		
+	}
+
+}
